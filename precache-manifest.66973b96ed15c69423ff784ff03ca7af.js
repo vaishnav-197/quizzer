fc90c08274102cf311596507b5410ec69a641014
vaishnav-197 <46111025+vaishnav-197@users.noreply.github.com>
2020-05-09 13:07:31 +0530

    Updates

---
+++ precache-manifest.66973b96ed15c69423ff784ff03ca7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c02e78483b799831284b4fc1ac8a2e3",
+    "url": "/quizzer/index.html"
+  },
+  {
+    "revision": "6d878c7213bdbc27d41a",
+    "url": "/quizzer/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "46cf4eef4a919e7f32ff",
+    "url": "/quizzer/static/js/2.e4b7235d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzer/static/js/2.e4b7235d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d878c7213bdbc27d41a",
+    "url": "/quizzer/static/js/main.44f13af1.chunk.js"
+  },
+  {
+    "revision": "96f90523ec7d2733f92b",
+    "url": "/quizzer/static/js/runtime-main.a8749c9e.js"
+  }
+]);
